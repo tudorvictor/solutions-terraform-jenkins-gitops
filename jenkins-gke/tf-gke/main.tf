871f65907ef3a28fb6991f864970b14c4528d5ae
Victor Tudor <victor.tudor@esempla.com>
2021-06-15 00:02:17 +0300

    change version helm chart,and comment timeout

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -191,7 +191,7 @@ resource "helm_release" "jenkins" {
   repository = "https://charts.helm.sh/stable"
   chart      = "jenkins"
 //  version    = "1.9.18"
-  timeout    = 1200
+ // timeout    = 2200
 
   values = [data.local_file.helm_chart_values.content]
 
