0981bd804fe195cf5697d328aaec8a92cb7cf9af
Victor Tudor <victor.tudor@esempla.com>
2021-06-14 23:32:59 +0300

    change version helm chart

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -190,7 +190,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.helm.sh/stable"
   chart      = "jenkins"
-  version    = "3.6.0"
+  version    = "3.3.22"
 //  version    = "1.9.18"
   timeout    = 1200
 
