8e781accb2eb9e326e274a869a342e126413774a
Tudor Victor <1tudor.victor@gmail.com>
2021-06-15 20:40:08 +0300

    V15 (#3)
    * add sensitive
    
    * change version helm chart
    
    * change version helm chart
    
    * change version helm chart
    
    * change version helm chart
    
    * change version helm chart
    
    * change version helm chart,and comment timeout
    
    * change version helm chart,and comment
    
    * change version helm chart,and commentc
    
    * change version helm chart,and commentcl
    
    * change version helm chart,and commentcl
    
    * change version helm chart,and commentcll
    
    Co-authored-by: Victor Tudor <1tudor.victor@gmail.com>

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -188,12 +188,10 @@ data "local_file" "helm_chart_values" {
 
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-//  repository = "https://charts.helm.sh/stable" //https://charts.jenkins.io
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "stable/jenkins"
 //  version    = "1.9.18"
-  timeout    = 1200
+  timeout    = 2400
 
   values = [data.local_file.helm_chart_values.content]
 
